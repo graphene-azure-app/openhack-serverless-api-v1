3cc0a258c052e4d70096d6123ee8bf8bfafa47c6
Will Li <luyuanli2010@gmail.com>
2021-10-11 17:44:34 -0700

    Delete redandant lines

--- Models/RatingModel.cs
+++ Models/RatingModel.cs
@@ -8,7 +8,7 @@ namespace SOH19DryRunFunctionApp.Models
         public string userId { get; set; }
         public string productId { get; set; }
         public DateTime timestamp { get; set; }
-        //public string timeStamp { get; internal set; }
+
         public string locationName { get; set; }
         public int rating { get; set; }
         public string userNotes { get; set; }
